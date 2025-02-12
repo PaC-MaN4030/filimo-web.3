315880c822ec049e3f761b1ab2dc20652ef4f957
PaC-MaN4030 <pacman40403030@gmail.com>
2025-02-12 22:17:10 +0330

    second commit

--- js/main.js
+++ js/main.js
@@ -63,4 +63,23 @@ async function swiper() {
       clickable: true,
     },
   });
+
+  var swiper = new Swiper(".comment", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 }
